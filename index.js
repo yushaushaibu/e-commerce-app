e1857c2f02d2689c787ea4198b6725203af59bdc
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-27 08:33:09 +0000

    form submission process

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi there, Enjoy..');
+    res.send(`
+            <div>
+                <form method="POST">
+                    <input name="email" placeholder="email">
+                    <input name="password" placeholder="password">
+                    <input name="passwordConfirmation" placeholder="password confirmation">
+                    <button>Sign up</button>
+                </form>
+            </div>
+    `)
 });
+
+app.post('/', (req, res) => {
+    res.send('Account created!!')
+})
 
 app.listen(3000, () => {
     console.log('Listening..')

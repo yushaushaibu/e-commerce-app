dbb082763629cbd2482058f690ecf1f7307244be
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-28 08:54:24 +0000

    getAll function refactor

--- repositories/users.js
+++ repositories/users.js
@@ -1,33 +1,32 @@
-const fs = require('fs');
+const fs = require("fs");
 
 class UsersRepository {
-    constructor(filename) {
-        if (!filename) {
-            throw new Error('Creating a new repository requires a filename');
-        }
-
-        this.filename = filename;
-
-        try {
-            fs.accessSync(this.filename);  // check if file exist
-        } catch (err) {
-
-            fs.writeFileSync(this.filename, '[]'); // writes/creates new file
-        }  
+  constructor(filename) {
+    if (!filename) {
+      throw new Error("Creating a new repository requires a filename");
     }
 
-    async getAll() {
-        const contents = await fs.promises.readFile(this.filename, { encoding: 'utf8'});  // reads entire content in file
+    this.filename = filename;
 
-        const data = JSON.parse(contents);  // convert data from JSON object to JS object
-        return data;  // return parsed data
+    try {
+      fs.accessSync(this.filename); // check if file exist
+    } catch (err) {
+      fs.writeFileSync(this.filename, "[]"); // writes/creates new file
     }
+  }
+
+  async getAll() {
+    // reads content in file, convert to JS object and return data
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
 }
 
 const test = async () => {
-    const repo = new UsersRepository('users.json');
-    const users = await repo.getAll();
-    console.log(users);
+  const repo = new UsersRepository("users.json");
+  const users = await repo.getAll();
+  console.log(users);
 };
 
 test();

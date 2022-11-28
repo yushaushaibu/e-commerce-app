05e30b43952b3956a275c9a3db51496ba13099c3
yushaushaibu <yushaushaibu1@gmail.com>
2022-11-28 09:17:52 +0000

    create function takes attrs argument which represent user data, writes the data to users.json

--- repositories/users.js
+++ repositories/users.js
@@ -21,11 +21,20 @@ class UsersRepository {
       await fs.promises.readFile(this.filename, { encoding: "utf8" })
     );
   }
+
+  // creates records. attrs is an object containing user form data
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records))
+  }
 }
 
 const test = async () => {
-  const repo = new UsersRepository("users.json");
-  const users = await repo.getAll();
+  const repo = new UsersRepository("users.json"); // access users repo
+  await repo.create({ email: 'test@test.com', password: 'password'}); // save new records to users.json
+  const users = await repo.getAll();  // get all users records
   console.log(users);
 };
 

990c8528f5115bb0a24ce4c59491d6fcf8ad5b1c
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-02 23:41:18 +0000

    COMPLETE IMPLEMENTATION OF EXPRESS SERVER, DATABASE AND USERS FILE

--- index.js
+++ index.js
@@ -47,39 +47,38 @@ app.post("/signup", async (req, res) => {
 
 app.get("/signout", (req, res) => {
   req.session = null;
-  res.send('You are logged out');
+  res.send("You are logged out");
 });
 
-app.get('/signin', (req, res) => {
+app.get("/signin", (req, res) => {
   res.send(`
       <form method="POST">
         <input name="email" placeholder="email">
         <input name="password" placeholder="password">
         <button>Sign In</button>
       </form>
-  `)
+  `);
 });
 
-app.post('/signin', async (req, res) => {
+app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
   const user = await usersRepo.getOneBy({ email });
-  
+
   if (!user) {
-    return res.send('User email not found');
+    return res.send("User email not found");
   }
 
   const validPassword = await usersRepo.comparePasswords(
     user.password,
     password
-  )
+  );
 
   if (!validPassword) {
-    return res.send('Invalid password');
+    return res.send("Invalid password");
   }
 
   req.session.userId = user.id;
-  res.send('You are signed in');
-
+  res.send("You are signed in");
 });
 
 app.listen(3000, () => {

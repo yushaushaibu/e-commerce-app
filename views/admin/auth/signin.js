540151ee5217bf28abc2cc7a81d52f468dd38b4d
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-03 08:36:28 +0000

    layout.js is a template for signin.js & signup.js file to render HTML basic syntax

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -1,9 +1,15 @@
+const layout = require('../layout');
+
 module.exports = () => {
-  return `
-        <form method="POST">
-            <input name="email" placeholder="email">
-            <input name="password" placeholder="password">
-            <button>Sign In</button>
-        </form>
-    `;
+  return layout({
+    content: `
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email">
+                <input name="password" placeholder="password">
+                <button>Sign In</button>
+            </form>
+        </div>
+  `
+  });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require("../layout");
+
 module.exports = ({ req }) => {
-  return `
-    Your ID is: <strong>${req.session.userId}</strong>
-              <div>
-                  <form method="POST">
-                      <input name="email" placeholder="email">
-                      <input name="password" placeholder="password">
-                      <input name="passwordConfirmation" placeholder="password confirmation">
-                      <button>Sign up</button>
-                  </form>
-              </div>
-    `;
+  return layout({
+    content: `
+        Your ID is: <strong>${req.session.userId}</strong>
+                    <div>
+                        <form method="POST">
+                            <input name="email" placeholder="email">
+                            <input name="password" placeholder="password">
+                            <input name="passwordConfirmation" placeholder="password confirmation">
+                            <button>Sign up</button>
+                        </form>
+                    </div>
+        `,
+  });
 };

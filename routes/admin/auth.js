ca6511f683ea271a3798d83dfae48ade43795576
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-07 06:37:45 +0000

    Validation in the signin post route handler

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -24,7 +24,7 @@ router.post(
     const { email, password, passwordConfirmation } = req.body;
 
     if (!errors.isEmpty()) {
-        return res.send(signupTemplate({ req, errors }));
+      return res.send(signupTemplate({ req, errors }));
     }
 
     const user = await usersRepo.create({ email, password });
@@ -43,25 +43,45 @@ router.get("/signin", (req, res) => {
   res.send(signinTemplate());
 });
 
-router.post("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await usersRepo.getOneBy({ email });
+router.post(
+  "/signin",
+  [
+    check("email")
+      .trim()
+      .normalizeEmail()
+      .isEmail()
+      .withMessage("Enter valid email")
+      .custom(async (email) => {
+        const user = await usersRepo.getOneBy({ email });
+        if (!user) {
+          throw new Error("Email not found!");
+        }
+      }),
+    check("password").trim(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
 
-  if (!user) {
-    return res.send("User email not found");
+    const { email, password } = req.body;
+    const user = await usersRepo.getOneBy({ email });
+
+    if (!user) {
+      return res.send("User email not found");
+    }
+
+    const validPassword = await usersRepo.comparePasswords(
+      user.password,
+      password
+    );
+
+    if (!validPassword) {
+      return res.send("Invalid password");
+    }
+
+    req.session.userId = user.id;
+    res.send("You are signed in");
   }
-
-  const validPassword = await usersRepo.comparePasswords(
-    user.password,
-    password
-  );
-
-  if (!validPassword) {
-    return res.send("Invalid password");
-  }
-
-  req.session.userId = user.id;
-  res.send("You are signed in");
-});
+);
 
 module.exports = router;

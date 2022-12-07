dee278a68a8f9cb0119681526526e2ae57e95cae
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-07 07:17:22 +0000

    helper.js function created

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -42,7 +42,7 @@ router.get("/signout", (req, res) => {
 });
 
 router.get("/signin", (req, res) => {
-  res.send(signinTemplate());
+  res.send(signinTemplate({}));
 });
 
 router.post(
@@ -52,7 +52,9 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.send(signinTemplate({ errors }));
+    }
 
     const { email } = req.body;
     const user = await usersRepo.getOneBy({ email });

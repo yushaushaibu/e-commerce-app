c94ac09e3f274ada41660282ac77d40424caeded
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-08 10:53:03 +0000

    product routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const authRouter = require('./routes/admin/auth');
+const productsRouter = require('./routes/admin/product');
 const { application } = require("express");
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(
 );
 
 app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("Listening..");

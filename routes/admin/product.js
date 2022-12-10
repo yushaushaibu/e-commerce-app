c367d8b80ff91895d3ab33b4efcb46bc16dbb772
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-10 13:04:11 +0000

    the edit button now working professionaly

--- routes/admin/product.js
+++ routes/admin/product.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 const multer = require("multer");
 
 const { requireAuth } = require("./middleware");
-const productRepo = require("../../repositories/products");
+const productsRepo = require("../../repositories/products");
 const newProductTemplate = require("../../views/admin/products/newProduct");
 const productsIndexTemplate = require("../../views/admin/products/index");
 const productEditTemplate = require("../../views/admin/products/edit");
@@ -14,7 +14,7 @@ const router = express.Router();
 const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", requireAuth, async (req, res) => {
-  const products = await productRepo.getAll();
+  const products = await productsRepo.getAll();
   res.send(productsIndexTemplate({ products }));
 });
 
@@ -36,14 +36,14 @@ router.post(
 
     const image = req.file.buffer.toString("base64");
     const { product, price } = req.body;
-    await productRepo.create({ product, price, image });
+    await productsRepo.create({ product, price, image });
 
     res.redirect("/admin/products");
   }
 );
 
 router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
-  const product = await productRepo.getOne(req.params.id);
+  const product = await productsRepo.getOne(req.params.id);
 
   if (!product) {
     return res.send("Product not found");
@@ -52,8 +52,31 @@ router.get("/admin/products/:id/edit", requireAuth, async (req, res) => {
   res.send(productEditTemplate({ product }));
 });
 
-router.post('/admin/products/:id/edit', requireAuth, async (req, res) => {
-  
-})
+router.post(
+  "/admin/products/:id/edit",
+  requireAuth,
+  [requireProduct, requirePrice],
+  upload.single("image"),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.send(productEditTemplate({ errors }));
+    }
+
+    const changes = req.body;
+    if (req.file) {
+      changes.image = req.file.buffer.toString("base64");
+    }
+
+    try {
+      await productsRepo.update(req.params.id, changes);
+    } catch (err) {
+      res.send("Could not find product");
+    }
+
+    res.redirect("/admin/products");
+  }
+);
 
 module.exports = router;

15d4a822836821f6c90fce060c8e215184a2e369
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-11 23:05:13 +0000

    delete product functionality added

--- views/admin/products/edit.js
+++ views/admin/products/edit.js
@@ -3,11 +3,31 @@ const layout = require("../layout");
 module.exports = ({ product }) => {
   return layout({
     content: `
-            <form method="POST">
-                <input type="text" name="product" value="${product.product}" />
-                <input type="text" name="price" value="${product.price}" />
-                <button>Submit</button>
-            </form>
-        `,
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Edit a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Product</label>
+              <input type="text" placeholder="Product" name="product" value="${product.product}" />
+            </div>
+
+            <div class="field">
+              <label class="label">Price</label>
+              <input value="${product.price}" class="input" placeholder="Price" name="price" />
+            </div>
+
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+
+            <button class="button is-primary">Edit</button>
+          </form>
+        </div>
+      </div>
+`,
   });
 };

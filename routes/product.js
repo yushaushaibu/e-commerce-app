de8011df2ba3cfdd199280c5bb6222676d50676c
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-11 23:43:05 +0000

    product view created to show list of products

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const productsRepo = require('../repositories/products');
+const productIndexTemplate = require('../views/products/index');
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  res.send("Home Page | Products");
+  const products = await productsRepo.getAll();
+
+  res.send(productIndexTemplate({ products }));
 });
 
 module.exports = router;

43bf9738f9efc5b4ee96d5ed1b6429eba656978a
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-12 00:25:51 +0000

    cart repo and cart routes created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const authRouter = require("./routes/admin/auth");
 const adminProductsRouter = require("./routes/admin/product");
 const productsRouter = require("./routes/product");
+const cartsRouter = require('./routes/carts');
 const { application } = require("express");
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(
 app.use(authRouter);
 app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 app.listen(3000, () => {
   console.log("Listening..");
---
+++ routes/carts.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+// POST request to add item to a cart
+
+// GET request to show all items in cart
+
+// DELETE request to remove item from a cart
+
+module.exports = router;

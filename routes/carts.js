43801f37d53735a3d10ae1166c73084be11b1b52
yushaushaibu <yushaushaibu1@gmail.com>
2022-12-12 22:03:37 +0000

    item and number of quantity added to cart

--- routes/carts.js
+++ routes/carts.js
@@ -19,11 +19,26 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
+  // Either add new item or add existing item to cart
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+
+  if (existingItem) {
+    // increment quantity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
   console.log(cart);
   res.send("Product Added to Cart");
 });
-
-// Either add new item or add existing item to cart
 
 // GET request to show all items in cart
 
